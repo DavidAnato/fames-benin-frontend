39d5400353bd32d7ca0468886c8635dc50a8e070
DavidAnato <david1anato@gmail.com>
2024-09-12 08:37:13 +0100

    Update NAv

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom'; 
+import { Link, NavLink } from 'react-router-dom';
 import famesLogo from '../assets/images/logos/fames-logo.png';
 import { useEffect, useState } from 'react';
 import useUserProfile from '../hooks/user';
@@ -49,7 +49,7 @@ const NavBar = () => {
 
   const handleLogout = () => {
     clearAuth();
-    navigate('/login');
+    navigate('/login');; // Redirect to login page after logout
   };
 
   const handleDropdownClick = () => {
@@ -66,12 +66,26 @@ const NavBar = () => {
         <div className="navbar-start flex items-center">
           <div className="dropdown lg:hidden">
             <div tabIndex={0} role="button" className="btn btn-ghost px-1" onClick={handleDropdownClick}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
               </svg>
             </div>
             {isDropdownOpen && (
-              <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
                 <li><NavLink to="/" className={({ isActive }) => `text-[16px] py-2 my-1 mx-1 rounded-xl hover:bg-gray-300 hover:rounded-full transition-all duration-500 ${isActive ? 'active' : ''}`} onClick={handleMenuItemClick}>{t('navbar.home')}</NavLink></li>
                 <li><NavLink to="/news" className={({ isActive }) => `text-[16px] py-2 my-1 mx-1 rounded-xl hover:bg-gray-300 hover:rounded-full transition-all duration-500 ${isActive ? 'active' : ''}`} onClick={handleMenuItemClick}>{t('navbar.news')}</NavLink></li>
                 <li><NavLink to="/opportunities" className={({ isActive }) => `text-[16px] py-2 my-1 mx-1 rounded-xl hover:bg-gray-300 hover:rounded-full transition-all duration-500 ${isActive ? 'active' : ''}`} onClick={handleMenuItemClick}>{t('navbar.opportunities')}</NavLink></li>
@@ -80,10 +94,21 @@ const NavBar = () => {
               </ul>
             )}
           </div>
-          <NavLink to="/" className="text-xl font-extrabold items-center text-red-700 ml-2 flex">
-            <img src={famesLogo} alt="FAMES Logo" className="h-10 mr-2" />
-            <span className="hidden md:inline">{t('navbar.logo')}</span>
-          </NavLink>
+            {user ? (
+            <NavLink to="/" className="text-xl font-extrabold items-center text-red-700 ml-2 flex">
+                <img src={famesLogo} alt="FAMES Logo" className="h-10 mr-2" />
+                <span className="hidden md:inline">
+                  {t('navbar.logo')}
+                </span>
+            </NavLink>
+            ) : (
+            <NavLink to="/" className="text-xl font-extrabold items-center text-red-700 ml-2 sm:flex hidden">
+                <img src={famesLogo} alt="FAMES Logo" className="h-10 mr-2" />
+                <span className="hidden md:inline">
+                  {t('navbar.logo')}
+                </span>
+            </NavLink>
+            )}
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -101,19 +126,33 @@ const NavBar = () => {
               <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-circle bg-gray-200" onClick={handleDropdownClick}>
                   <div className="w-10 rounded-full flex items-center justify-center">
-                    <img id="user-photo" className="w-10 h-10 rounded-full object-cover" alt={t('navbar.userAvatarAlt')} src={user.profile_picture || user.picture_url} />
+                    {user.profile_picture || user.picture_url ? (
+                      <img
+                        id='user-photo'
+                        className="w-10 h-10 rounded-full object-cover"
+                        alt={t('navbar.userAvatarAlt')}
+                        src={user.profile_picture || user.picture_url}
+                      />
+                    ) : (
+                      <i className="fa fa-user text-2xl" aria-hidden="true"></i>
+                    )}
                   </div>
                 </div>
                 {isDropdownOpen && (
-                  <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 p-2 shadow">
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 p-2 shadow"
+                  >
                     <li>
                       <NavLink to="/profile" className={({ isActive }) => `mx-1 rounded-xl hover:bg-gray-300 hover:rounded-full transition-all duration-500 ${isActive ? 'active' : ''}`} onClick={handleMenuItemClick}>
-                        <i className="fas fa-user text-xl"></i>{t('navbar.profile')}
+                        <i className="fas fa-user text-xl"></i>
+                        {t('navbar.profile')}
                       </NavLink>
                     </li>
                     <li>
                       <button onClick={() => { handleLogout(); handleMenuItemClick(); }} className="mx-1 rounded-xl hover:bg-red-300 hover:rounded-full transition-all duration-500">
-                        <i className="fas fa-sign-out-alt text-xl"></i>{t('navbar.logout')}
+                        <i className="fas fa-sign-out-alt text-xl"></i>
+                        {t('navbar.logout')}
                       </button>
                     </li>
                   </ul>
@@ -125,12 +164,13 @@ const NavBar = () => {
             <div className="flex space-x-2 items-center">
               <NavLink to="/login" className="btn btn-ghost font-bold shadow rounded-full btn-sm">{t('navbar.login')}</NavLink>
               <NavLink to="/register" className="btn btn-accent font-bold shadow shadow-emerald-500/50 rounded-full btn-sm">{t('navbar.signup')}</NavLink>
+              <TranslationDropdown isDrop bgColor="bg-gray-100" />
             </div>
           )}
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default NavBar;
